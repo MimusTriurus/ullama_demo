297900c46b0339b1745ce1869d0fcd8c28ca27b1
SSS <stendhal.syndrome.studio@gmail.com>
2025-08-05 22:00:17 +0100

    init repository

---
+++ Source/ullama_demo/ullama_demoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ullama_demoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULLAMA_DEMO_API Aullama_demoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
